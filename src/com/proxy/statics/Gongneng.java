f9a859bf378228ee4ebec8f58741969b94e83475
darth-xiaozhijun <15717061273@163.com>
2019-08-11 22:55:27 +0800

    静态代理

---
+++ src/com/proxy/statics/Gongneng.java
@@ -0,0 +1,7 @@
+package com.proxy.statics;
+
+public interface Gongneng{
+	void zhidingxiaomubiao();
+	
+	void chifan();
+}
---
+++ src/com/proxy/statics/Women.java
@@ -0,0 +1,8 @@
+package com.proxy.statics;
+
+public class Women {
+	public static void main(String[] args) {
+		MiShu mishu = new MiShu();
+		mishu.zhidingxiaomubiao();
+	}
+}
