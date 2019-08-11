535cd8f862f9674d415c18fd129e3db78b89a711
darth-xiaozhijun <15717061273@163.com>
2019-08-11 22:58:02 +0800

    cglib实现动态代理

---
+++ src/com/proxy/dynamic/cglib/Laozong.java
@@ -0,0 +1,13 @@
+package com.proxy.dynamic.cglib;
+
+public class Laozong {
+	
+	public void chifan() {
+		System.out.println("吃饭");
+	}
+
+	public void mubiao() {
+		System.out.println("目标");
+	}
+
+}
---
+++ src/com/proxy/dynamic/cglib/Women.java
@@ -0,0 +1,15 @@
+package com.proxy.dynamic.cglib;
+
+import net.sf.cglib.proxy.Enhancer;
+
+public class Women {
+	public static void main(String[] args) {
+		Enhancer enhancer = new Enhancer();
+		enhancer.setSuperclass(Laozong.class);
+		enhancer.setCallback(new Mishu());
+		
+		Laozong laozong = (Laozong) enhancer.create();
+		laozong.chifan();
+		
+	}
+}
